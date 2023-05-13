5ec7bf06163a073dec9251d7358d85bd7111ca9a
redm27 <130718216+redm27@users.noreply.github.com>
2023-05-13 22:18:58 +0900

    반대의 퀴즈 프로그램
    영어 한글을 내고 영어를 맞추는 프로그램 또한 구현

--- TESTQUIZ.c
+++ TESTQUIZ.c
@@ -39,6 +39,25 @@ int quiz(Voca *v[], int index){
     return 1;
 }
 
+int quiz2 (Voca *v[], int index){
+    srand(time(NULL)); // 난수 생성을 위한 시드 설정
+    int randomIndex = rand() % index; // 랜덤한 인덱스 선택
+
+    printf("뜻 : %s\n", v[randomIndex]->meaning);
+
+    char answer[10];
+    printf("뜻을 입력하세요: ");
+    scanf("%s", answer);
+
+    if (strcmp(answer, v[randomIndex]->word) == 0) {
+        printf("정답입니다!\n");
+    } else {
+        printf("오답입니다. 정답은 %s입니다.\n", v[randomIndex]->word);
+    }
+
+    return 1;
+}
+
 int main(void){
   Voca *v[100];
   int index = 0;
