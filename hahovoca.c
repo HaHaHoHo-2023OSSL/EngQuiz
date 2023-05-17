92d4b9b6faa2a7533706f89c35114c74f82933d6
redm27 <130718216+redm27@users.noreply.github.com>
2023-05-17 23:46:56 +0900

    V3 | 단어장에서 퀴즈 출제 함수 추가
    V3 | 단어장에서 퀴즈 출제 함수 추가

--- hahovoca.c
+++ hahovoca.c
@@ -15,7 +15,6 @@ typedef struct{
     char *filename;
 } User;
 
-
 // Create
 int addWord(Voca *v){
     fflush(stdin);
@@ -70,6 +69,151 @@ int deleteWord(Voca *v){
     v->word[0] = '\0';
     v->meaning[0] = '\0';
     printf("=> 삭제 완료!\n");
+
+    return 1;
+}
+
+// 메뉴 선택 함수 1
+int selectMenu_1(){
+    int menu;
+    
+    printf("\n\u256D\u2500\u2500 HaHoVOCA Login Page \u2500\u2500\u256E\n");
+    printf("\u07CB                         \u07CB\n");
+    printf("      1| 회원가입\n");
+    printf("      2| 로그인\n");
+    printf("      0| 프로그램 종료\n");
+    printf("\u07CB                         \u07CB");
+    printf("\n\u2570\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u256F\n");
+    printf(">> ");
+
+    scanf(" %d", &menu);
+    getchar();  // 엔터 중복 입력 방지
+    
+    return menu;
+}
+
+// 메뉴 선택 함수 2
+int selectMenu_2(){
+    int menu;
+    
+    // printf("\n*** HaHoVOCA ***\n");
+    printf("\n\u256D\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500 HaHoVOCA \u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u256E\n");
+    printf("\u07CB                                            \u07CB\n");
+    printf("  1| 단어장 조회 ");
+    printf(" 2| 단어 추가 ");
+    printf(" 3| 단어 수정\n");
+    printf("  4| 단어 삭제   ");
+    printf(" 5| 단어 검색 ");
+    printf(" 6| 단어 저장\n");
+    printf("  7| 퀴즈 1      ");
+    printf(" 8| 퀴즈 2    ");
+    printf(" 0| 로그아웃\n");
+    printf("\u07CB                                            \u07CB");
+    printf("\n\u2570\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u256F\n");
+    printf(">> ");
+
+    scanf(" %d", &menu);
+    getchar();  // 엔터 중복 입력 방지
+    
+    return menu;
+}
+
+// 데이터 번호 선택 함수
+int selectDataNo(Voca *v[], int index){
+    int num;
+    listWord(v, index);
+
+    printf("번호 입력 (취소 0) : ");
+    scanf("%d", &num);
+
+    return num;
+}
+
+// 이름 검색 함수
+void searchName(Voca *v[], int index){
+    int scnt = 0;
+    char search[20];
+
+    printf("검색 단어 : ");
+    scanf("%s", search);
+
+    printf("\nNo  Word  Meaning\n");
+    printf("====================\n");
+    
+    int count = 0;
+    for(int i = 0; i < index; i++){
+        if(v[i] == NULL) continue;
+        if(strstr(v[i]->word, search)){
+            count++;
+            printf("%2d ", count);
+            readWord(*v[i]);
+            scnt = 1;
+        }
+    }
+    
+    if (scnt == 0) printf("=> 검색된 데이터 없음!\n");
+    printf("\n");
+}
+
+// 퀴즈 1
+int quiz1 (Voca *v[], int index){
+
+    if (index == 0){
+        printf("저장된 단어가 없습니다.");
+        return 1;
+    }
+
+    srand(time(NULL)); // 난수 생성을 위한 시드 설정
+    int randomIndex = rand() % index; // 랜덤한 인덱스 선택
+
+    while(v[randomIndex]->word == NULL){ // delete로 인해 NULL 값이 나온경우
+        srand(time(NULL)); // 시드 재설정
+        randomIndex = rand() % index; //랜덤 인덱스 재선택
+    }
+
+    printf("단어 : %s\n", v[randomIndex]->word);
+
+    char answer[10];
+    printf("뜻을 입력하세요: ");
+    scanf("%s", answer);
+
+    if (strcmp(answer, v[randomIndex]->meaning) == 0) {
+        printf("정답입니다!\n");
+    } else {
+        printf("오답입니다. 정답은 %s입니다.\n", v[randomIndex]->meaning);
+    }
+
+    return 1;
+}
+
+// 퀴즈 2
+int quiz2 (Voca *v[], int index){
+
+    if (index == 0){
+        printf("저장된 단어가 없습니다.");
+        return 1;
+    }
+
+    srand(time(NULL)); // 난수 생성을 위한 시드 설정
+    int randomIndex = rand() % index; // 랜덤한 인덱스 선택
+
+    while(v[randomIndex]->word == NULL){ // delete로 인해 NULL 값이 나온경우
+        srand(time(NULL)); // 시드 재설정
+        randomIndex = rand() % index; //랜덤 인덱스 재선택
+    }
+
+
+    printf("뜻 : %s\n", v[randomIndex]->meaning);
+
+    char answer[10];
+    printf("영어 철자를 입력하세요: ");
+    scanf("%s", answer);
+
+    if (strcmp(answer, v[randomIndex]->word) == 0) {
+        printf("정답입니다!\n");
+    } else {
+        printf("오답입니다. 정답은 %s입니다.\n", v[randomIndex]->word);
+    }
 
     return 1;
 }
