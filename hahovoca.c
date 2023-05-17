afcb5e1baa16ae28595de3aff645b418f4b4a48e
redm27 <130718216+redm27@users.noreply.github.com>
2023-05-17 23:38:55 +0900

    V1-1 | CRUD 함수 구현
    V1-1 | CRUD 함수 구현

--- hahovoca.c
+++ hahovoca.c
@@ -14,3 +14,62 @@ typedef struct{
     Voca *voca[50];
     char *filename;
 } User;
+
+
+// Create
+int addWord(Voca *v){
+    fflush(stdin);
+    printf("단어 : ");
+    scanf("%s", v->word);
+        
+    fflush(stdin);
+    printf("의미 : ");
+    scanf("%s", v->meaning);
+
+    return 1;
+}
+
+// Read
+void readWord(Voca v){
+    printf("%s : %s\n", v.word, v.meaning);
+}
+
+// Read (다중 데이터)
+void listWord(Voca *v[], int index){
+    printf("\nNo  Word  Meaning\n");
+    printf("====================\n");
+    
+    int count = 0;
+    for(int i = 0; i < index; i++){
+        if(v[i] == NULL) continue;
+        count++;
+        printf("%2d  ", count);
+        readWord(*v[i]);
+    }
+    printf("\n");
+}
+
+// Update
+int updateWord(Voca *v){
+    fflush(stdin);
+    printf("새 단어 : ");
+    scanf("%s", v->word);
+        
+    fflush(stdin);
+    printf("새 의미 : ");
+    scanf("%s", v->meaning);
+
+    printf("=> 수정 성공!\n");
+
+    return 1;
+}
+
+// Delete
+int deleteWord(Voca *v){
+
+    v->word[0] = '\0';
+    v->meaning[0] = '\0';
+    printf("=> 삭제 완료!\n");
+
+    return 1;
+}
